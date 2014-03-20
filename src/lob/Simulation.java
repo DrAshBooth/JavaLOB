51f0a070be4c44acaa1d0ad90e7b1d7b0128a62e
ab24v07 <ash.booth@soton.ac.uk>
2014-03-20 18:25:06 +0000

    First commit

---
+++ src/lob/Simulation.java
@@ -0,0 +1,12 @@
+package lob;
+
+public class Simulation {
+	
+	public static void run() {
+		System.out.println("Beginning simulation...");
+	}
+	
+	public static void main(String[] args) {
+		run();
+	}
+}
